25c95141b4fed9e59fe252ef7df9c07e047a28dc
fangxiaopeng <850899969@qq.com>
2019-12-29 10:44:04 +0800

    提交Param注解

---
+++ frouter/frouter_annotation/src/main/java/com/fxp/frouter/annotation/Param.java
@@ -0,0 +1,27 @@
+package com.fxp.frouter.annotation;
+
+/**
+ * Title:       Param
+ * <p>
+ * Package:     com.fxp.frouter.annotation
+ *
+ * @Author: fxp
+ * <p>
+ * Create at:   2019-12-28 16:50
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2019-12-28    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public @interface Param {
+
+    String name() default "";
+
+}
