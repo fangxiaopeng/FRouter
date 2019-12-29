f0b34241243839aba6f7005050d8092c86b16100
fangxiaopeng <850899969@qq.com>
2019-12-29 10:47:52 +0800

    @Param注解示例

--- app/src/main/java/com/fxp/frouter/MainActivity.java
+++ app/src/main/java/com/fxp/frouter/MainActivity.java
@@ -5,9 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.fxp.frouter.annotation.FRouter;
+import com.fxp.frouter.annotation.Param;
 
 @FRouter(group = "app", path = "/app/MainActivity")
 public class MainActivity extends AppCompatActivity {
+
+    @Param
+    String userId;
+
+    @Param(name = "age")
+    int userAge;
+
+    @Param(name = "isHappy")
+    boolean isHappy = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
